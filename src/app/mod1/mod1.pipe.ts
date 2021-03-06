c0e4b30076645cbfbbc06db70f166bb131e93684
Artem <aprricod@gmail.com>
2021-03-06 15:59:09 +0300

    fixed

--- src/app/mod1/mod1.pipe.ts
+++ src/app/mod1/mod1.pipe.ts
@@ -4,7 +4,16 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'mod1',
 })
 export class Mod1Pipe implements PipeTransform {
-  transform(values: string, ...args: unknown[]): unknown {
-    return values.toString().replace(',', '; ');
+  transform(values: string[], ...args: unknown[]): string {
+    const mapValue: string[] = values.map((value) => {
+      return (value);
+    });
+    return mapValue.join('; ');
   }
 }
+
+
+
+// transform(values: string, ...args: unknown[]): unknown {
+//   return values.toString().replace(',', '; ');
+// }
